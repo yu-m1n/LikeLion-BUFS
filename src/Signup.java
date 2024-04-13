85dd932ebf9bebaa14a315cd90c866f6f3813afe
yumin <rdddq01@gmail.com>
2024-04-14 02:30:37 +0900

    LikeLion sign up personal project

--- src/Signup.java
+++ src/Signup.java
@@ -1,7 +1,6 @@
 import java.io.*;
-import java.util.*;
 
-public class Sighup {
+public class Signup {
     public static void main(String[] args) throws IOException {
         boolean signup = false;
 
@@ -27,27 +26,33 @@ public class Sighup {
                 System.out.print("아이디를 입력해주세요: ");
                 String id = br.readLine();
 
-                //bw.write("입력하신 아이디는 "+id+"입니다."); //입력한 아이디 확인하는 기능 만들기
+                //bw.write("입력하신 아이디는 "+id+"입니다."); //입력한 아이디 확인하는 기능 만들기?
 
                 System.out.print("비밀번호를 입력해주세요: ");
                 String pw = br.readLine();
 
                 for(int i = 0; i < 6; i++ ){
-                    System.out.print("비밀번호 확인을 위해 한 번 더 입력해주세요: ");
-                    String pw2 = br.readLine();
-/////////////////////////////////비밀번호 확인 추가하기///////////////////////////////////////////////////////
+                    String pw2 = br.readLine(); //???????????? 이걸 어디에 넣어야할꼬 현 상태: 비밀번호 입력 받고 확인 상태가 아닌데도 그냥 입력 받으려고 함
+                    if(pw != pw2){ //아님 여기가 잘못 되었나
+                        System.out.print("비밀번호 확인을 위해 한 번 더 입력해주세요: ");
+                        //////////비밀번호 확인 추가하기//////////////////////
+                    }
+                    else{
+                        System.out.print("생년월일 6자리를 입력해주세요: ");
+                        String birth = br.readLine();
+
+                        System.out.print("이메일을 입력해주세요: ");
+                        String mail = br.readLine();
+
+                        System.out.println("회원 가입이 완료 되었습니다. 감사합니다.");
+                        //System.out.println("당신의 아이디는" ) //저장된 값들 보여주기
+                    }
                 }
-
-                System.out.print("생년월일 6자리를 입력해주세요: ");
-                String birth = br.readLine();
-
-                System.out.print("이메일을 입력해주세요: ");
-                String mail = br.readLine();
-            }
             }
         }
 
         //br.close();
+    }
     }
 
 
