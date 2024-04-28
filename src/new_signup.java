703796e1e00e9da5d826e5cbeb5d9e28414d26f9
yumin <rdddq01@gmail.com>
2024-04-28 23:44:24 +0900

    LikeLion sign up project

---
+++ src/new_signup.java
@@ -0,0 +1,10 @@
+public class new_signup {
+    public class Hash{
+        public Hash(Integer size){
+            this.hashTable = new Slot[size]
+        }
+    }
+    public static void main(String[] args){
+
+    }
+}
