fe98fc85396d91d43a63f66302b403f7d7ec7f00
yumin <rdddq01@gmail.com>
2024-04-29 18:39:56 +0900

    LikeLion sign up project

--- src/Signup.java
+++ src/Signup.java
@@ -1,11 +1,15 @@
 import java.io.*;
 
-public class Signup {
+public class Signup { //진행과정에서 수정.. + 해시맵 쓰기
     public static void main(String[] args) throws IOException {
         boolean signup = false;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String id;
+        String pw;
+        String birth;
+        String mail;
 
         while(!signup){
             System.out.print("회원가입을 하시겠습니까?(Y/N): ");
@@ -24,35 +28,61 @@ public class Signup {
                 //signup = false;
                 System.out.println("회원가입을 계속 진행하겠습니다.");
                 System.out.print("아이디를 입력해주세요: ");
-                String id = br.readLine();
+                id = br.readLine();
 
-                //bw.write("입력하신 아이디는 "+id+"입니다."); //입력한 아이디 확인하는 기능 만들기?
+                System.out.println("당신의 아이디는 " +id+ " 입니다");
 
                 System.out.print("비밀번호를 입력해주세요: ");
-                String pw = br.readLine();
+                pw = br.readLine();
 
-                for(int i = 0; i < 6; i++ ){
-                    String pw2 = br.readLine(); //???????????? 이걸 어디에 넣어야할꼬 현 상태: 비밀번호 입력 받고 확인 상태가 아닌데도 그냥 입력 받으려고 함
-                    if(pw != pw2){ //아님 여기가 잘못 되었나
-                        System.out.print("비밀번호 확인을 위해 한 번 더 입력해주세요: ");
-                        //////////비밀번호 확인 추가하기//////////////////////
-                    }
-                    else{
-                        System.out.print("생년월일 6자리를 입력해주세요: ");
-                        String birth = br.readLine();
+                System.out.print("비밀번호 확인을 위해 한 번 더 입력해주세요: ");
+                String rpw = br.readLine();
+                int cnt = 5; //
 
-                        System.out.print("이메일을 입력해주세요: ");
-                        String mail = br.readLine();
+                if(!pw.equals(rpw)){ //pw와 rpw 비밀번호가 일치하지 않을 경우
+                    for (int i = 5; i > 0; i--) {
+                        System.out.println("이전에 입력한 비밀번호와 다릅니다.");
+                        System.out.print("비밀번호를 다시 한 번 더 입력해주세요: ");
+                        rpw = br.readLine();
 
-                        System.out.println("회원 가입이 완료 되었습니다. 감사합니다.");
-                        //System.out.println("당신의 아이디는" ) //저장된 값들 보여주기
+
+                        if(pw.equals(rpw)) { //기회를 얻은 후 비밀번호가 일치하는 경우
+                            System.out.println("다음 단계로 넘어가겠습니다.");
+                            break;
+                        }
+                        if(!pw.equals(rpw)) { //기회 한 번 틀린 경우(반복)
+                            cnt--;
+                            System.out.println("남은 비밀번호 작성 횟수는 " + cnt+ " 입니다.");
+
+                            if(!pw.equals(rpw)){ //기회 모두 소진
+                                System.out.println("5번의 기회를 모두 틀렸습니다.");
+                                System.out.println("회원가입이 종료되었습니다.");
+                                break;
+                            }
+                        }
                     }
                 }
             }
+            else{
+                //System.out.println("당신의 비밀번호는 " +pw+ " 입니다."); //보완
+                System.out.print("생년월일 6자리를 입력해주세요: ");
+                birth = br.readLine();
+
+                System.out.print("이메일을 입력해주세요: ");
+                mail = br.readLine();
+
+                //System.out.println("당신의 아이디는 " + id + " 입니다"); //저장된 값들 보여주기 ->
+                //System.out.println("비밀번호는 " + pw + " 입니다");
+                System.out.println("당신의 생년월일은 " + birth +" 입니다.");
+                System.out.println("당신의 이메일은 " + mail +" 입니다.");
+
+                System.out.println("회원 가입이 완료 되었습니다. 감사합니다.");
+
+                signup = true;
+                if(signup == true) {
+                    break;
+                }
+            }
         }
-
-        //br.close();
     }
-    }
-
-
+}
